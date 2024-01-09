0561ae9892589ad99076ab98a3a99a5ddbad2f97
Felipe Rodrigues <105247997+FlpsRodri@users.noreply.github.com>
2024-01-09 11:38:21 -0400

    get key - correção e layout
    Correção de bug do espaço em branco, e criação da interface grafica Concluido

--- get key WIFI on windows/getKey.py
+++ get key WIFI on windows/getKey.py
@@ -1,13 +1,50 @@
 import os
 import time
+from tkinter import *
+
 class app():
     def __init__(self):
         
-        #root = Tk()
-        #root.title("GetKey - version LP1.0")
-        #root.wm_attributes("-toolwindow",1)
-        #root.geometry("400
-        #root.mainloop()
+        root = Tk()
+        root.title("GetKey - version LP1.1")
+        root.wm_attributes("-toolwindow",1)
+        root.resizable(0,0)
+        Label(root, text="""
+░░░░░░░░░░░░░░░░░░░░░█████████░░░░░░░░░
+░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███░░░░░░░
+░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███░░░░
+░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░
+░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███░
+░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██
+░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██
+░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██
+██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██
+█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██
+██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░
+░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░░
+░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█░░░░░
+░░████████████░░░█████████████████░░░░░░""", fg="red", bg="#000").pack()
+        
+        os.system("ver > temp.txt")
+        with open("temp.txt","r") as version:
+            lang = "en" if "version" in (version.read()).lower() else "pt"
+        self.get_users()
+        try:os.system("del key.txt")
+        except Exception as ERROR: print(ERROR)
+        for SSID in self.SSID_list:
+            self.hacKey(SSID,lang)
+        root.mainloop()
+        
+    def get_users(self):
+        def filter_ssid(ssid):
+            for i in ssid:
+                if i == " ":
+                    ssid.replace(i,"")
+                else:
+                    for i in reversed(ssid):
+                        if i == " ": ssid.replace(i,"")
+                        else: return ssid[1:]
         os.system('netsh wlan show profile | find ":" > rd.txt')
         time.sleep(1)
         try:
@@ -16,38 +53,32 @@ class app():
         except FileNotFoundError:
             print("File Not Found")
             return
-        os.system('del rd.txt')
-        SSID_list = []
-        Redes = {}
+        #os.system('del rd.txt')
+        self.SSID_list = []
         for i in rd:
             i = i.split(":")
             if len(i[-1]) > 0:
-                SSID_list.append(i[-1])
-        for SSID in SSID_list:
-            SSID = SSID.replace(" ","")
-            command = f'netsh wlan show profile "{SSID}" key=clear | find /i "chave" > temp.txt' 
+                i = filter_ssid(i[-1])
+                self.SSID_list.append(i)
+    def hacKey(self,ssid,lang):
+        if lang == "pt":
+            command = f'netsh wlan show profile "{ssid}" key=clear | find /i "chave" > temp.txt' 
             os.system(command)
             with open("temp.txt","r") as temp:
                 temp = str(temp.read()).replace(" ","")
-            Redes[SSID] = temp
-            os.system('del temp.txt')
-        #for i in Redes: print(i, Redes[i])
-
+        else:
+            command = f'netsh wlan show profile "{ssid}" key=clear | find /i "key content" > temp.txt' 
+            os.system(command)
+            with open("temp.txt","r") as temp:
+                temp = str(temp.read()).replace(" ","")
+        key = temp.split(":")[-1]
+        if len(key) < 8: key = "None?\n"
+        os.system('del temp.txt | del rd.txt')
         try:
-            with open("key.txt","r"):
-                k = True
-            os.system("del key.txt")
-            with open("key.txt","a") as key:
-                for i in Redes:
-                    txt  = i+" : "+ Redes[i]
-                    key.write(txt+"\n")
-                    print(i,Redes[i])
-        except:
-            
-            with open("key.txt","a") as key:
-                txt  = i+Redes[i]
-                key.write(txt)
-                
-            
+            with open("key.txt","a") as key_file:
+                txt  = ssid+" : "+ key
+                key_file.write(txt)
+        except Exception as ERROR: print(ERROR)
+                            
 if __name__ == "__main__":
     run = app()
